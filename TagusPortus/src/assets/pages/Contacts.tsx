8e17264aae0580f187bcfe2db4383173d5b6a760
MarChurra <126945275+MarChurra@users.noreply.github.com>
2024-09-11 19:09:42 +0100

    Update Contacts.tsx

--- TagusPortus/src/assets/pages/Contacts.tsx
+++ TagusPortus/src/assets/pages/Contacts.tsx
@@ -123,7 +123,7 @@ const Contacts: React.FC = () => {
           />
           <textarea
             name="request"
-            value={userEmail}
+            value={userRequest}
             placeholder='Escreva aqui o seu pedido'
             onChange={(e) => setUserRequest(e.target.value)}
             required
